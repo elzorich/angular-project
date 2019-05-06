cfd809ba9aa623c3e48f13c4c3d5a8235706138c
el_zorich <elenazorich@gmail.com>
2019-05-06 22:56:34 +0200

    add path

--- ngx-app/src/app/main-list/main-list.component.ts
+++ ngx-app/src/app/main-list/main-list.component.ts
@@ -12,6 +12,7 @@ export class MainListComponent implements OnInit {
 
   public tours$: Observable<Tour[]> = tours$;
   public selectTour: string;
+  public background = 'assets/images/1.jpg';
 
 
   constructor() { }
